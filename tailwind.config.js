c5ef461d6c8c2c6aa04b10353e765f1a5a29ee9c
sahil <sahilsaify7860@gmail.com>
2022-06-23 20:23:54 +0530

    Structure Completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    "./pages/**/*.{js, jsx}",
+    "./components/**/*.{js, jsx}",
+  ],
   content: [],
   theme: {
     extend: {},
